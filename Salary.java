f93226a911c9aee287a5bf81d49d6c1f4fc004b2
Bhagyashri-57 <patilcbhagya@gmail.com>
2026-06-25 21:05:12 +0530

    JavaPrograms

---
+++ Salary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double basic = sc.nextDouble();
+
+        double hra = basic * 0.20;
+        double da = basic * 0.10;
+
+        double gross = basic + hra + da;
+
+        System.out.println("Gross Salary = " + gross);
+    }
+}
